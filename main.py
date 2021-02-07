b7500b166535b81a992cad0657ce7e47121c398e
Zakaria Abdisalam <47097630+zabdisalam@users.noreply.github.com>
2021-02-07 13:33:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from keys import *
 
 client = discord.Client()
 
-driver = webdriver.Chrome()
+driver = None
 
 def login(email, password, condition):
 	global driver
@@ -39,8 +39,6 @@ def login(email, password, condition):
 			print("condition = false now cuz why not")		
 	return condition
 
-
-
 	
 def alert():
 	global driver
@@ -58,8 +56,10 @@ def alert():
 
 @client.event
 async def on_message(message):
+	global driver
 	server_id = client.get_guild(807703919768895529)
 	if "!setup" in message.content and len(message.content.split()) == 3 :
+		driver = webdriver.Chrome()
 		splitted_message = message.content.split()
 		username = splitted_message[1]
 		password = splitted_message[2]
@@ -72,21 +72,12 @@ async def on_message(message):
 		alert()
 		await message.channel.send(file=discord.File('img/scanned_notifs.png'))
 		os.remove('img/scanned_notifs.png')
+		driver.close()
 	elif "!setup" not in message.content or len(message.content.split()) != 3 :
 		message.channel.send("```The message format is incorrect```.")
 
 	if cond2 == False:
 		await message.channel.send(f"{message.author.mention} You have not authenticated in time! Please restart the process.")
-
-
-
-
-
-
-
-
-
-
 
 
 

00dbc340466c8b66a4c024cfb6a4cb62254fe4db
zabdisalam <kofigarebulasp@gmail.com>
2021-02-07 15:19:30 -0500

    last push

--- main.py
+++ main.py
@@ -14,78 +14,75 @@ client = discord.Client()
 driver = None
 
 def login(email, password, condition):
-	global driver
-	driver.get('https://uottawa.brightspace.com/d2l/home')
-	time.sleep(5)
-	emailInput = driver.find_element_by_id("i0116")
-	emailInput.send_keys(email)
-	emailInput.send_keys(Keys.ENTER)
-	time.sleep(5)
-	passwordInput = driver.find_element_by_id("passwordInput")
-	passwordInput.send_keys(password)
-	time.sleep(2)
-	passwordInput.send_keys(Keys.ENTER)
-	start = time.time()
-	time.sleep(4)
-	while time.time() - start < 61:
-		print(time.time()-start)
-		try:
-			driver.find_element_by_id("idSIButton9").click()
-			break
-		except:
-			pass
-		if (time.time() - start) > 59:
-			condition = False
-			print("condition = false now cuz why not")		
-	return condition
+    global driver
+    driver.get('https://uottawa.brightspace.com/d2l/home')
+    time.sleep(5)
+    emailInput = driver.find_element_by_id("i0116")
+    emailInput.send_keys(email)
+    emailInput.send_keys(Keys.ENTER)
+    time.sleep(5)
+    passwordInput = driver.find_element_by_id("passwordInput")
+    passwordInput.send_keys(password)
+    time.sleep(2)
+    passwordInput.send_keys(Keys.ENTER)
+    start = time.time()
+    time.sleep(4)
+    while time.time() - start < 61:
+        print(time.time()-start)
+        try:
+            driver.find_element_by_id("idSIButton9").click()
+            break
+        except:
+            pass
+        if (time.time() - start) > 59:
+            condition = False
+            print("condition = false now cuz why not")        
+    return condition
 
-	
+    
 def alert():
-	global driver
-	time.sleep(2)
-	x = True
-	while x:
-		try:
-			pyautogui.click('bell.png')
-			x = False
-		except:
-			pass
-	l = True
-	while l:
-		coords = pyautogui.locateOnScreen('scan.png', confidence = 0.5)
-		if coords != None:
-			print(coords)
-			pyautogui.screenshot('img/scanned_notifs.png', region = coords)
-			l = False
-
+    global driver
+    time.sleep(2)
+    x = True
+    while x:
+        try:
+            pyautogui.click('bell.png')
+            x = False
+        except:
+            pass
+    l = True
+    while l:
+        coords = pyautogui.locateOnScreen('scan.png', confidence = 0.5)
+        if coords != None:
+            print(coords)
+            pyautogui.screenshot('img/scanned_notifs.png', region = coords)
+            l = False
 
 
 @client.event
 async def on_message(message):
-	global driver
-	server_id = client.get_guild(807703919768895529)
-	if "!setup" in message.content and len(message.content.split()) == 3 :
-		driver = webdriver.Chrome()
-		splitted_message = message.content.split()
-		username = splitted_message[1]
-		password = splitted_message[2]
-		cond = True
-		try:
-			cond2 = login(username, password, cond)
-			
-		except:
-			print("Something went wrong! Please restart the setup process.")
-		alert()
-		await message.channel.send(file=discord.File('img/scanned_notifs.png'))
-		os.remove('img/scanned_notifs.png')
-		driver.close()
-	elif "!setup" not in message.content or len(message.content.split()) != 3 :
-		await message.channel.send("```The message format is incorrect```.")
+    global driver
+    server_id = client.get_guild(807703919768895529)
+    if "!setup" in message.content and len(message.content.split()) == 3 :
+        driver = webdriver.Chrome()
+        splitted_message = message.content.split()
+        username = splitted_message[1]
+        password = splitted_message[2]
+        cond = True
+        cond2 = login(username, password, cond)
+        print(cond2)
 
-	if cond2 == False:
-		await message.channel.send(f"{message.author.mention} You have not authenticated in time! Please restart the process.")
+        if cond2 == True:
+            alert()
+            await message.channel.send(file=discord.File('img/scanned_notifs.png'))
+            os.remove('img/scanned_notifs.png')
+            driver.close()
 
+        elif cond2 == False:
+            await message.channel.send(f"{message.author.mention} You have not authenticated in time! Please restart the process.")
 
+    elif "!setup" in message.content and len(message.content.split()) != 3:
+        await message.channel.send("Fix message format")
 
 print('BrightAlert bot is running!!')
 client.run(token)
